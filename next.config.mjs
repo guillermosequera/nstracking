9e897c32ef23942bab75d1a1e344e751be12b199
guillermosequera <sequeracrespo@gmail.com>
2024-09-14 12:01:36 -0300

    Fix: deploy by ui and webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: '/api/:path*',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
+  webpack: (config) => {
+    const path = require('path');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+export default nextConfig;
