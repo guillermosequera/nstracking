cac8d6b747373d80d0093aea8dd0763cc844dd65
guillermosequera <sequeracrespo@gmail.com>
2025-01-13 16:23:07 -0300

    fix UI

--- src/hooks/useDelayedJobs.js
+++ src/hooks/useDelayedJobs.js
@@ -5,21 +5,18 @@ import { fetchDelayedJobs } from '@/utils/jobUtils'
 import { queryUtils } from '@/config/queryConfig'
 
 export function useDelayedJobs() {
-  const { data: trabajosAtrasados, isLoading, error, refetch } = useQuery({
+  const { data, isLoading, error, refetch } = useQuery({
     queryKey: queryUtils.generateQueryKey('delayed-jobs'),
     queryFn: async () => {
       console.log('Fetching delayed jobs...')
       const response = await fetchDelayedJobs()
       console.log('Delayed jobs fetched successfully')
-      return response
-    },
-    staleTime: 0, // Considerar los datos obsoletos inmediatamente
-    cacheTime: 0, // No cachear la respuesta
-    refetchOnMount: true // Refetch al montar el componente
+      return response.data
+    }
   })
 
   return {
-    trabajosAtrasados,
+    data,
     isLoading,
     error,
     refetch
--- src/hooks/useProductionJobs.js
+++ src/hooks/useProductionJobs.js
@@ -3,21 +3,18 @@ import { fetchProductionJobs } from '@/utils/jobUtils'
 import { queryUtils } from '@/config/queryConfig'
 
 export function useProductionJobs() {
-  const { data: trabajosAgrupados, isLoading, error, refetch } = useQuery({
+  const { data, isLoading, error, refetch } = useQuery({
     queryKey: queryUtils.generateQueryKey('production-jobs'),
     queryFn: async () => {
       console.log('Fetching production jobs...')
       const response = await fetchProductionJobs()
       console.log('Production jobs fetched successfully')
-      return response
-    },
-    staleTime: 0, // Considerar los datos obsoletos inmediatamente
-    cacheTime: 0, // No cachear la respuesta
-    refetchOnMount: true // Refetch al montar el componente
+      return response.data
+    }
   })
 
   return {
-    trabajosAgrupados,
+    trabajosAgrupados: data,
     isLoading,
     error,
     refetch
